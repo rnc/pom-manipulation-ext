d73fc694a644f14ae47ce4d049fc3c736c6ee35f
Nick Cross <ncross@redhat.com>
2026-01-16 17:05:20 +0000

    Update to new PNC project-sources plugin. Remove metadata plugin

--- integration-test/src/it/project-sources-injection-basic/verify.groovy
+++ integration-test/src/it/project-sources-injection-basic/verify.groovy
@@ -21,14 +21,10 @@ def pom = new XmlSlurper().parse( pomFile )
 def plugin = null
 pom.build.plugins.children().each{
     if (it.artifactId == "project-sources-maven-plugin" ){
-        assert it.version == '1.0'
-    }
-    else if (it.artifactId == 'build-metadata-maven-plugin'){
-        assert it.version == '1.3.1'
+        assert it.version == '2.0'
     }
 }
 
-assert new File( basedir, 'build.metadata' ).exists()
 assert new File( basedir, 'target/project-sources-injection-basic-1-project-sources.tar.gz').exists()
 
 
